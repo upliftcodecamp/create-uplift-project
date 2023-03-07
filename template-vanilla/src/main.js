8ef068d096a43842ebc0ab8850257b49c29aa138
Jay Are Galinada <jayaregalinada@gmail.com>
2023-03-07 11:40:46 +0800

    feat: Added template-vanilla

---
+++ template-vanilla/src/main.js
@@ -0,0 +1,5 @@
+/**
+ * Insert your javascript codes here
+ */
+
+console.log('Hello World! 👋');
---
+++ template-vanilla/vite.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import path from 'node:path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  root: './src',
+});
